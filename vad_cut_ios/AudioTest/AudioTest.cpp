6cf2c04a8a667b66a0199ea32f1ad5a63033a1ab
wangxudong <wangxudong@wangxudongdeMacBook-Pro.local>
2020-11-25 12:28:34 +0800

    update AudioTest.cpp

--- vad_cut_ios/AudioTest/AudioTest.cpp
+++ vad_cut_ios/AudioTest/AudioTest.cpp
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/uio.h>
+#include <iostream>
+#include <iomanip>
 #include "INS.h"
 #include "IVad.h"
 #include "WavReader.h"
 #include "WavWriter.h"
+
+using namespace std;
 
 int main_test_wav_cut(int argc, char *argv[])
 {
 #define NN 160
 #define Cut_Samples_length (65*16000)
 #define Cut_Location (60*16000)
+#define Wav_End_Length (6*16000)
 #define Cut_Time 1
 	char input_path[128];
 	memset(input_path, 0, sizeof(input_path));
@@ -19,18 +23,19 @@ int main_test_wav_cut(int argc, char *argv[])
 	FILE *file;
 	if( (file = fopen(input_path, "rb") )== NULL)
 	{
-	    printf("Input File Error.\n");
+	    cerr << "Input File Error." << endl;
             return 1;   
 	}	
 	fclose(file);
-#if 0
-        if(access(argv[1], 0) == -1)
+#if 0        
+	if(access(argv[1], 0) == -1)
         {
-            printf("Output Path Error.\n");
+            cerr << "Output Path Error." <<endl;
             return 1;
         }	
 #endif
-	printf("%s\n", argv[0]);
+	cout<<"-----Wav Split Start-----" <<endl;
+	
 	char file_format[5];
 	for(int i= strlen(input_path) - 4; i<strlen(input_path); i++)
 	{
@@ -39,15 +44,13 @@ int main_test_wav_cut(int argc, char *argv[])
 	}
 
 	file_format[4] = '\0';
-        printf("%s\n", file_format);
 
 	if(strcmp(file_format,".wav") != 0)
 	{
-	    printf("File Format Error.\n");
+	    cerr<< "File Format Error." <<endl;
 	    return 1;
 	}
 	
-
 
         WavReader wav_reader;
         wav_reader.Open(argv[0]);
@@ -59,7 +62,7 @@ int main_test_wav_cut(int argc, char *argv[])
  
         if (((wav_reader.m_nBitsPerSample >> 3) * channel) == 0)
         {
-                printf("wav Read Error\n");
+                cerr<< "wav Read Error." <<endl;
                 return 1;
         }
         else
@@ -73,11 +76,12 @@ int main_test_wav_cut(int argc, char *argv[])
 	}
 
 	int cut_times = mic_samples/Cut_Samples_length;
-	int frames = mic_samples/NN;
 
-	printf("mic_samples = %d \n", mic_samples);
-	printf("frames = %d\n", frames);
-	printf("cut times = %d\n", cut_times);	
+	if( ( (mic_samples  - cut_times*Cut_Samples_length) < Wav_End_Length ) &&
+             ( cut_times >0 ) )
+        {
+            cut_times = cut_times - 1;
+        }
 
         WavWriter wav_write;
 
@@ -111,11 +115,9 @@ int main_test_wav_cut(int argc, char *argv[])
 
 	len = strlen(wav_out);
 	wav_out[len] = '\0';
-	printf("1---%s\n", wav_out);
 
 	if(cut_times > 0)
 	{
-//NS Init
             void *pNS_inst = NULL;
             int ns_Mode = 3;
 	    int vad_Mode = 2;
@@ -127,7 +129,6 @@ int main_test_wav_cut(int argc, char *argv[])
 	    int vad = 3;
 	    fvad_new(&pVad_inst, sampleRate, vad_Mode);
 //Cut Process
-	    printf("-----Init End-----\n");
 	    int start = 0, end = 0, loops = 0;
 	    int cut_num = 0;
 	    int max_loop = 500;
@@ -151,8 +152,6 @@ int main_test_wav_cut(int argc, char *argv[])
 	       
 	       if( (vad == 0) || (loops > max_loop))
 	       {
-		   printf("-----VAD = 0 or loop > max-----\n");
-	
 		   snprintf(str1, sizeof(str1), "%d", cut_num);
 		   cut_times--;
 		   wav_out[len] = '_';
@@ -173,7 +172,8 @@ int main_test_wav_cut(int argc, char *argv[])
 	 	       strcat(wav_out, str2);
 	 	       wav_out[strlen(str1)+len+4+1] = '\0';
 		   }
-	 	   printf("%s\n", wav_out);
+
+	 	   cout << "Wav File Path:: " << wav_out << endl;
 //save wav
    		  short *output_cut_pt = NULL;
 		  output_cut_pt = (short *)malloc(sizeof(short)*(end - steps + Cut_Location));
@@ -181,8 +181,10 @@ int main_test_wav_cut(int argc, char *argv[])
                  for(int i = steps - Cut_Location; i < end; i ++) {
                      output_cut_pt[i-(steps - Cut_Location)] = mic_input_pt[i];
                   }
-
-		  printf("End Samples Location = %d\n",end);
+		  
+		  float end_time = (float)( end - (steps - Cut_Location) )/16000;
+		
+		  cout << "Current Wav Duration:: "<< fixed << setprecision(2) << end_time << endl;
 
          	  wav_write.Open(wav_out, 16000, bits_per_sample, channel);
 	          wav_write.Write(output_cut_pt, (end - steps + Cut_Location)*channel);
@@ -198,7 +200,14 @@ int main_test_wav_cut(int argc, char *argv[])
 		  if(cut_times == 0)
 		  {
 			cut_times = (mic_samples - end)/Cut_Samples_length;
+			
+			if( ( ( (mic_samples - end) - cut_times*Cut_Samples_length) < Wav_End_Length ) &&
+			    ( cut_times >0 ) )
+			{
+			    cut_times = cut_times - 1;
+			}
 		  }
+
 	       }
 	       else
 	       {
@@ -228,7 +237,8 @@ int main_test_wav_cut(int argc, char *argv[])
               strcat(wav_out, str2);
               wav_out[strlen(str1)+len+4+1] = '\0';
 	   }
-           printf("%s\n", wav_out);
+
+	   cout << "Wav File Path:: " << wav_out << endl;
 
 //Save Wav
            short *output_cut_pt = NULL;
@@ -238,24 +248,28 @@ int main_test_wav_cut(int argc, char *argv[])
                output_cut_pt[i - end] = mic_input_pt[i];
            }
 
+           float end_time = (float)( mic_samples - end )/16000;
+           cout << "Current Wav Duration:: " << fixed <<setprecision(2) << end_time << endl;
+
            wav_write.Open(wav_out, 16000, bits_per_sample, channel);
            wav_write.Write(output_cut_pt, (mic_samples - end)*channel);
            wav_write.Close();
  //free
 	   fns_free(pNS_inst);
 	   fvad_free(pVad_inst);
-	   printf("Cut\n");
 	}
 	else
 	{
 
             strcat(wav_out, str2);
             wav_out[strlen(str1)+len+4] = '\0';
-            printf("%s\n", wav_out);
+ 	    cout << "Wav File Path:: " << wav_out << endl;
+            float end_time = (float)mic_samples/16000;
+            cout << "Current Wav Duration:: " << fixed << setprecision(2) << end_time << endl;
+
 	    wav_write.Open(wav_out, 16000, bits_per_sample, channel);
             wav_write.Write(mic_input_pt, mic_samples*channel);
             wav_write.Close();
-	    printf("No Cut\n");
 	}
 	
 	free(mic_input_pt);
@@ -271,17 +285,16 @@ int main(int argc, char* argv[])
         res = main_test_wav_cut(argc-1, &argv[1]);
         if(res == 0)
         {
-	    printf("Cut Success\n");
+	    cout << "-----Split Success-----" << endl;
         }
         else
         {
-	    printf("Cut Error\n");
+	    cerr << "-----Cut Error-----" << endl;
         }
     }
     else
     {
-	printf(" Input Param Error. Use info:\n");
-	printf("./AudioTest input_file_path output_path\n");
+	cerr << " Input Param Error. Use info:: ./AudioTest input_file_path output_path" << endl;
     }
 
     return 0;
